cd5a70a7b03fb0d8163aeadb578aa16d35b1b4e9
Praveen Kumar <praveen.peddachitti@gmail.com>
2020-02-11 14:44:01 +0530

    working code

---
+++ CricketPlayersViperDemo/TeamDetails/Presenter/TeamDetailsPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  TeamDetailsPresenter.swift
+//  CricketPlayersViperDemo
+//
+//  Created by Adaps on 11/02/20.
+//  Copyright © 2020 CrazyTricks. All rights reserved.
+//
+
+import Foundation
---
+++ CricketPlayersViperDemo/TeamDetails/View/PlayerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlayerTableViewCell.swift
+//  CricketPlayersViperDemo
+//
+//  Created by Adaps on 11/02/20.
+//  Copyright © 2020 CrazyTricks. All rights reserved.
+//
+
+import UIKit
+
+class PlayerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
